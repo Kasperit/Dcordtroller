03bc208f018ee1c3e3b1f4f9f46432a07dee4872
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2018-09-19 10:25:15 +0300

    Fix footer and header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './index.css';
 import HeaderBar from './client/components/Navigation/Header/Header'
 import ListUserContainers from './client/containers/ListUser/ListUserContainers'
+import FooterBar from './client/components/Navigation/Footer/footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HeaderBar/>
-        <div className="content">
-          <ListUserContainers/>
-        </div>
-        <div className="footer">
-        </div>
+          <HeaderBar/>
+          <div className="content">
+              <ListUserContainers/>
+          </div>
+          <FooterBar/>
       </div>
     );
   }
---
+++ src/client/components/Navigation/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FooterBar = (props) => {
+    return (
+        <div className="footer">
+            Footer Area
+        </div>
+    )
+}
+
+export default FooterBar
