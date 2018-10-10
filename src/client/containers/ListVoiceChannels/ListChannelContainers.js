f7b2ab58dcf1e19716692361e71e80a56304e350
Khoa Pham <kenjifam@gmail.com>
2018-10-10 06:39:43 +0300

    style sb

--- src/client/containers/ListVoiceChannels/ListChannelContainers.js
+++ src/client/containers/ListVoiceChannels/ListChannelContainers.js
@@ -1,66 +1,71 @@
-import React, { Fragment, Component } from 'react'
-import { Col, Row } from 'antd'
-import ListServer from '../../components/ListServer/ListServer'
-import ListVoiceChannel from '../../components/ListVoiceChannel/ListVoiceChannel'
+import React, { Fragment, Component } from "react";
+import { Col, Row } from "antd";
+import ListServer from "../../components/ListServer/ListServer";
+import ListVoiceChannel from "../../components/ListVoiceChannel/ListVoiceChannel";
 
 class ListChannelContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            server: null,
-            listOfGuilds: this.props.listOfGuilds
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      server: null,
+      listOfGuilds: this.props.listOfGuilds
+    };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      !(this.state.server === nextState.server) ||
+      this.state.listOfGuilds !== nextState.listOfGuilds
+    );
+  }
+
+  handlePlayMusic = item => {
+    let voiceChannelName = item.item.name; // XD
+    let serverName = item.server;
+    let url = item.url;
+    console.log(url);
+
+    let listOfGuilds = [...this.state.listOfGuilds];
+    for (let i = 0; i < listOfGuilds.length; i++) {
+      if (listOfGuilds[i].server === serverName) {
+        let generalObject = listOfGuilds[i].serverObject.channels.array()[1];
+        generalObject.send(
+          "!" + voiceChannelName + ";" + serverName + ";" + "<" + url + ">"
+        );
+      }
     }
+  };
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return !(this.state.server === nextState.server) || this.state.listOfGuilds !== nextState.listOfGuilds
+  render() {
+    let { listOfGuilds, server } = this.state;
+    let listOfVoice = null;
+    let listServer = [];
+    if (server) {
+      listOfVoice = (
+        <ListVoiceChannel
+          listOfGuilds={listOfGuilds}
+          server={server}
+          handlePlayMusic={server => this.handlePlayMusic(server)}
+          closeUserList={() => this.setState({ server: null })}
+        />
+      );
     }
-
-    handlePlayMusic = (item) => {
-        let voiceChannelName = item.item.name;      // XD
-        let serverName = item.server;
-        let url = item.url;
-        console.log(url);
-
-        let listOfGuilds = [...this.state.listOfGuilds];
-        for (let i = 0; i < listOfGuilds.length; i++) {
-            if (listOfGuilds[i].server === serverName) {
-                let generalObject = listOfGuilds[i].serverObject.channels.array()[1];
-                generalObject.send("!" + voiceChannelName + ";" + serverName + ";" + "<" + url + ">");
-            }
-        }
+    for (let i = 0; i < listOfGuilds.length; i++) {
+      listServer.push(listOfGuilds[i].server);
     }
-
-
-    render() {
-        let { listOfGuilds, server } = this.state;
-        let listOfVoice = null;
-        let listServer = [];
-        if (server) {
-            listOfVoice =
-                <ListVoiceChannel
-                    listOfGuilds={listOfGuilds}
-                    server={server}
-                    handlePlayMusic={(server) => this.handlePlayMusic(server)}
-                    closeUserList={() => this.setState({ server: null })}
-                />
-        }
-        for (let i = 0; i < listOfGuilds.length; i++) {
-            listServer.push(listOfGuilds[i].server)
-        }
-        return (
-            <Fragment>
-                <Row>
-                    <ListServer
-                        listServer={listServer}
-                        server={server}
-                        chooseServer={(server) => this.setState({ server })}
-                    />
-                    {listOfVoice}
-                </Row>
-            </Fragment>
-        )
-    }
+    return (
+      <Fragment>
+        <Row style={{ height: "100%" }}>
+          <ListServer
+            listServer={listServer}
+            server={server}
+            chooseServer={server => this.setState({ server })}
+          />
+          {listOfVoice}
+        </Row>
+      </Fragment>
+    );
+  }
 }
 
 export default ListChannelContainer;
--- src/client/containers/ListVoiceChannels/ListVoiceChannelsContainers.js
+++ src/client/containers/ListVoiceChannels/ListVoiceChannelsContainers.js
@@ -1,46 +1,46 @@
-import React,{Component} from 'react';
-import ListChannelContainer from './ListChannelContainers'
-import {Spin,Icon,Col,Row} from 'antd'
+import React, { Component } from "react";
+import ListChannelContainer from "./ListChannelContainers";
+import { Spin, Icon, Col, Row } from "antd";
 
-class ListVoiceChannelContainers extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            listOfGuilds: null
-        }
-    }
-    shouldComponentUpdate(nextProps,nextState){
-        return !(nextProps.location.path === this.props.location.path
-                && nextState === this.state)
-    }
+class ListVoiceChannelContainers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listOfGuilds: null
+    };
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return !(
+      nextProps.location.path === this.props.location.path &&
+      nextState === this.state
+    );
+  }
 
-    componentDidMount(){
-        this.props.infoFromDiscord ? this.setState({listOfBannedWords: ["TEST","TEST1"]}) : this.setState({listOfBannedWords: ["TEST","TEST1"]})
-    }
+  componentDidMount() {
+    this.props.infoFromDiscord
+      ? this.setState({ listOfBannedWords: ["TEST", "TEST1"] })
+      : this.setState({ listOfBannedWords: ["TEST", "TEST1"] });
+  }
 
-    render(){
-        const iconLoading = <Icon type="loading" style={{ fontSize: 40 }} spin />;
-        const {infoFromDiscord} = this.props;
-        if(infoFromDiscord){
-            return (
-                <div>
-                    <Col span={16}>
-                        <ListChannelContainer
-                            listOfGuilds={infoFromDiscord}
-                        />
-                    </Col>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Spin indicator={iconLoading}/>
-                </div>
-            )
-        }
-
+  render() {
+    const iconLoading = <Icon type="loading" style={{ fontSize: 40 }} spin />;
+    const { infoFromDiscord } = this.props;
+    if (infoFromDiscord) {
+      return (
+        <div className="content-server">
+          <Col className="content-server-col24" span={24}>
+            <ListChannelContainer listOfGuilds={infoFromDiscord} />
+          </Col>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Spin indicator={iconLoading} />
+        </div>
+      );
     }
+  }
 }
 
 export default ListVoiceChannelContainers;
-
