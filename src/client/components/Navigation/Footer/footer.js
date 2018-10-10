f781e8ee36aa7612291ed2174b44b0a74d7518a9
Khoa Pham <kenjifam@gmail.com>
2018-10-10 03:32:05 +0300

    layout

--- src/client/components/Navigation/Footer/footer.js
+++ src/client/components/Navigation/Footer/footer.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
+const FooterBar = props => {
+  return (
+    <div className="footer">
+      <p>
+        &copy; Copyright 2018 by{" "}
+        <Link className="login-link" to="/main">
+          <b>Dcordtroller Team</b>
+        </Link>
+        . All rights reserved.
+      </p>
+    </div>
+  );
+};
 
-const FooterBar = (props) => {
-    return (
-        <div className="footer">
-
-        </div>
-    )
-}
-
-export default FooterBar
+export default FooterBar;
--- src/client/components/Navigation/Header/Header.js
+++ src/client/components/Navigation/Header/Header.js
@@ -1,22 +1,21 @@
 import React, { Fragment } from "react";
-import NavigationItems from './NavigationItems/NavigationItems'
-import { NavLink } from 'react-router-dom'
-import './Header.css'
+import NavigationItems from "./NavigationItems/NavigationItems";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
-const headerBar = (props) => {
-    return (
-        <div className={'header'}>
-            <div className='logo'>
-                <NavLink to="/main">
-                    <h1>Dcordtroller</h1>
-                </NavLink>
-            </div>
-            <div className="nav-bar-container">
-                <NavigationItems {...props} />
-            </div>
-        </div>
-    )
+const headerBar = props => {
+  return (
+    <div className={"header"}>
+      <div className="main-logo">
+        <NavLink to="/main">
+          <h1>Dcordtroller</h1>
+        </NavLink>
+      </div>
+      <div className="nav-bar-container">
+        <NavigationItems {...props} />
+      </div>
+    </div>
+  );
 };
-
 
 export default headerBar;
